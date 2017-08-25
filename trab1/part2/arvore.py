ca30c30e1cec66b7beb9af3fb51431bcd43a14c7
Matheus Nerone <matheus.agio@hotmail.com>
2017-08-25 14:07:03 -0300

    changes

--- trab1/part2/arvore.py
+++ trab1/part2/arvore.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python
 # -*- encoding: iso-8859-1 -*-
 
-import sys
+import os
 from sklearn import cross_validation
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
 
 
-
 def main(X_data, y_data, test_size):
-    # splits data
-	#print "Spliting data..."
-	X_train, X_test, y_train, y_test =  cross_validation.train_test_split(X_data, y_data, test_size=test_size)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        X_data, y_data, test_size=test_size
+    )
 
-	# cria uma DT
-	clf  = tree.DecisionTreeClassifier()
+    # cria uma DT
+    clf = tree.DecisionTreeClassifier()
 
-	clf.fit(X_train, y_train)
+    clf.fit(X_train, y_train)
 
-	# predicao do classificador
-	y_pred = clf.predict(X_test)
+    # predicao do classificador
+    y_pred = clf.predict(X_test)
 
-	print 'score:', clf.score(X_test, y_test)
+    print 'score:', clf.score(X_test, y_test)
 
-	# cria a matriz de confusao
-	return confusion_matrix(y_test, y_pred)
+    # cria a matriz de confusao
+    return confusion_matrix(y_test, y_pred)
+
 
 if __name__ == "__main__":
-	sizes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-	X_data, y_data = load_svmlight_file('./data')
-	for x in sizes:
-		print 'test size:', x
-		print main(X_data, y_data, x)
-		print '-----------------------------------------'
+    sizes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    X_data, y_data = load_svmlight_file(os.getcwd() + '/./data')
+    for x in sizes:
+        print 'test size:', x
+        print main(X_data, y_data, x)
+        print '-----------------------------------------'
--- trab1/part2/nb.py
+++ trab1/part2/nb.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python
 # -*- encoding: iso-8859-1 -*-
 
-import sys
-import numpy
+import os
 from sklearn import cross_validation
 from sklearn.datasets import load_svmlight_file
 from sklearn.metrics import confusion_matrix
-from sklearn import preprocessing
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import GaussianNB
-
-
-import os
 
 
 def main(X_data, y_data, test_size):
-	X_train, X_test, y_train, y_test =  cross_validation.train_test_split(X_data, y_data, test_size=test_size)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        X_data, y_data, test_size=test_size
+    )
 
-	X_train = X_train.toarray()
-	# cria o classificador
+    X_train = X_train.toarray()
 
-	### Note that GaussianNB wont fit in this kind of sparse data (a lot of zero features)
-	#gnb  = GaussianNB()
-	gnb  = BernoulliNB()
+    # cria o classificador
+    gnb = BernoulliNB()
 
-	gnb.fit(X_train, y_train)
+    gnb.fit(X_train, y_train)
 
-	# predicao do classificador
-	y_pred = gnb.predict(X_test)
+    # predicao do classificador
+    y_pred = gnb.predict(X_test)
 
-	print 'score:', gnb.score(X_test, y_test)
+    print 'score:', gnb.score(X_test, y_test)
 
-	return confusion_matrix(y_test, y_pred)
+    return confusion_matrix(y_test, y_pred)
 
 
 if __name__ == "__main__":
-	sizes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-	X_data, y_data = load_svmlight_file('./data')
-	for x in sizes:
-		print 'test size:', x
-		print main(X_data, y_data, x)
-		print '-----------------------------------------'
+    sizes = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    X_data, y_data = load_svmlight_file(os.getcwd() + '/./data')
+    for x in sizes:
+        print 'test size:', x
+        print main(X_data, y_data, x)
+        print '-----------------------------------------'
