e5eb7b2bcbac347a57cd8e2e12b0ad6d5c6e1f6f
Matheus Nerone <matheus.agio@hotmail.com>
2017-09-06 15:28:27 -0300

    wip

--- trab2/confusion_matrix.py
+++ trab2/confusion_matrix.py
@@ -4,9 +4,9 @@ import scikitplot.plotters as skplt
 
 
 if __name__ == '__main__':
-    _, real_labels = load_svmlight_file('./test_base')
+    _, real_labels = load_svmlight_file('./test')
 
-    with open('./libsvm-3.22/tools/test_base.predict') as f:
+    with open('./test.predict') as f:
         predicted_labels = f.readlines()
 
     predicted_labels = [float(x.strip()) for x in predicted_labels]
