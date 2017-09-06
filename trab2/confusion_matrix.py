f45aebf6a2712f21bfd4ff4a1164a329b3b74756
Matheus Nerone <matheus.agio@hotmail.com>
2017-09-06 14:35:31 -0300

    wip

---
+++ trab2/confusion_matrix.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_svmlight_file
+import matplotlib.pyplot as plt
+import scikitplot.plotters as skplt
+
+
+if __name__ == '__main__':
+    _, real_labels = load_svmlight_file('./test_base')
+
+    with open('./libsvm-3.22/tools/test_base.predict') as f:
+        predicted_labels = f.readlines()
+
+    predicted_labels = [float(x.strip()) for x in predicted_labels]
+
+    skplt.plot_confusion_matrix(
+        real_labels,
+        predicted_labels,
+        text_fontsize="large"
+    )
+    plt.savefig('./confusion-matrix.png', bbox_inches='tight')
