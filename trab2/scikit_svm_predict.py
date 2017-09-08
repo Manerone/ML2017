1057eb7feb9dc0a0474233a1b1a91fd60471d5c9
Matheus Nerone <matheus.agio@hotmail.com>
2017-09-08 17:40:01 -0300

    wip

---
+++ trab2/scikit_svm_predict.py
@@ -0,0 +1,13 @@
+from sklearn.svm import SVC
+from sklearn.datasets import load_svmlight_file
+
+
+if __name__ == '__main__':
+    c = 8.0
+    gamma = 2
+    train_attrs, train_labels = load_svmlight_file('./treino.vet')
+    test_attrs, test_labels = load_svmlight_file('./teste.vet')
+
+    classificator = SVC(C=c, gamma=gamma)
+    classificator.fit(train_attrs, train_labels)
+    print 'Prediction score:', classificator.score(test_attrs, test_labels)
