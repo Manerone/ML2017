26769d179a7a12f800f9305e738a679655b7f3b9
Matheus Nerone <matheus.agio@hotmail.com>
2017-09-09 13:57:03 -0300

    wip

--- trab2/confusion_matrix.py
+++ trab2/confusion_matrix.py
@@ -3,22 +3,13 @@ import matplotlib.pyplot as plt
 import scikitplot.plotters as skplt
 
 
-if __name__ == '__main__':
-    _, real_labels = load_svmlight_file('./teste.vet')
-
-    file_path = './teste.vet.predict'
-    title = 'LibSVM Confusion Matrix'
-    save_path = 'confusion_matrixes/libsvm.png'
-
-    # file_path = './scikit_svm_predicts'
-    # title = 'Scikit Confusion Matrix'
-    # save_path = 'confusion_matrixes/scikit.png'
-
+def build_matrix(file_path, title, save_path, real_labels):
     with open(file_path) as f:
         predicted_labels = f.readlines()
 
     predicted_labels = [float(x.strip()) for x in predicted_labels]
 
+    plt.figure()
     skplt.plot_confusion_matrix(
         real_labels,
         predicted_labels,
@@ -27,3 +18,19 @@ if __name__ == '__main__':
         title=title
     )
     plt.savefig(save_path, bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    _, real_labels = load_svmlight_file('./teste.vet')
+
+    file_path = './teste.vet.predict'
+    title = 'LibSVM Confusion Matrix'
+    save_path = 'confusion_matrixes/libsvm.png'
+
+    build_matrix(file_path, title, save_path, real_labels)
+
+    file_path = './scikit_svm_predicts'
+    title = 'Scikit Confusion Matrix'
+    save_path = 'confusion_matrixes/scikit.png'
+
+    build_matrix(file_path, title, save_path, real_labels)
