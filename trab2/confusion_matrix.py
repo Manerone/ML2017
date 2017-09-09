0d274c9a03185e588436b4a0d76166f3f6e634cc
Matheus Nerone <matheus.agio@hotmail.com>
2017-09-09 12:56:16 -0300

    wip

--- trab2/confusion_matrix.py
+++ trab2/confusion_matrix.py
@@ -9,11 +9,16 @@ if __name__ == '__main__':
     with open('./teste.vet.predict') as f:
         predicted_labels = f.readlines()
 
+    # with open('./scikit_svm_predicts') as f:
+    #     predicted_labels = f.readlines()
+
     predicted_labels = [float(x.strip()) for x in predicted_labels]
 
     skplt.plot_confusion_matrix(
         real_labels,
         predicted_labels,
-        text_fontsize="large"
+        text_fontsize="large",
+        normalize=True,
+        title='LibSVM Confusion Matrix'
     )
-    plt.savefig('./confusion-matrix.png', bbox_inches='tight')
+    plt.savefig('./confusion-matrix-libsvm.png', bbox_inches='tight')
